f51743735d874e5133aa6316cba86339303f2389
Benjamin Hall <hallb1016@gmail.com>
2021-01-30 04:44:23 -0500

    Update SecurityInfo.cs

--- VotingBot/SecurityInfo.cs
+++ VotingBot/SecurityInfo.cs
@@ -7,8 +7,8 @@ namespace VotingBot
         public const string token = ""; // Bot token
         public const string botID = ""; // Bot discriminator (four numbers after the #)
         public const ulong botClient = 0; // Client ID
-        public const string botName = "[bot name]";
+        public const string botName = "Voting Bot";
         public const string botVersion = "1.0.0.0";
-        public static readonly Color botColor = new Color(217, 217, 217);
+        public static readonly Color botColor = new Color(33, 94, 166);
     }
 }
